9bf27d0149013cd3c04b88a224b241d5261bf8aa
gruzilla <grillen@abendstille.at>
2014-03-12 21:52:38 +0100

    added hydrabundle-dependency, added it to kernel and routing

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
             new ViennaPhp\ViennaPhpBundle\ViennaPhpBundle(),
+            new Hydra\HydraBundle\HydraBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
